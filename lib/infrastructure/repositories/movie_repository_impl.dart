c43f6e4c6c328898deee420dc0d05f77a39be41d
julioale21 <30907777+julioale21@users.noreply.github.com>
2023-09-27 20:50:27 -0300

    added new providers

---
+++ lib/infrastructure/repositories/movie_repository_impl.dart
@@ -0,0 +1,14 @@
+import 'package:cinemapedia/config/domain/datasources/movies_datasource.dart';
+import 'package:cinemapedia/config/domain/entities/movie.dart';
+import 'package:cinemapedia/config/domain/repositories/movies_repository.dart';
+
+class MovieRepositoryImpl extends MoviesRepository {
+  final MoviesDatasource datasource;
+
+  MovieRepositoryImpl(this.datasource);
+
+  @override
+  Future<List<Movie>> getNowPlayigMovies({int page = 1}) {
+    return datasource.getNowPlayingMovies(page: page);
+  }
+}
